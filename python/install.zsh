786de6313bebb280ed51f2dc8852c3029833eb9c
Cristian Perez Jensen <cristianpjensen@gmail.com>
2021-04-15 19:38:51 +0200

    python install script
    Installs python3 with pip3 for either linux or macOS. Modules can be
    added to customise the install.

---
+++ python/install.zsh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+main() {
+  echo "installing python"
+
+  if [[ $OSTYPE =~ ^darwin ]]; then
+    brew install python3
+    pip3 install --upgrade pip
+  fi
+  if [[ $OSTYPE =~ ^linux ]]; then
+    sudo apt-get install python3 pip
+  fi
+
+  echo "installing python modules"
+
+  # add modules, if you want any other modules
+  local modules
+  modules=(
+    virtualenv
+    jedi
+    yapf
+    flake8
+    notebook
+  )
+  for module in $modules; do
+    pip3 install $module
+  done
+}
+
+main $@
